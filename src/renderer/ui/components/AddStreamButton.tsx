463f0445437946efa4c38c54c32e252bc0050cd9
Bassam Helal <basshelal@gmail.com>
2020-10-22 18:04:43 +0100

    Converting AddStreamButton.tsx to class Component

--- src/renderer/ui/components/AddStreamButton.tsx
+++ src/renderer/ui/components/AddStreamButton.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from "react"
+import React, {Component} from "react"
 import {Button} from "@material-ui/core"
 import Dialog from "@material-ui/core/Dialog"
 import DialogTitle from "@material-ui/core/DialogTitle"
@@ -9,101 +9,106 @@ import {sendToMain} from "../UICommons"
 import {Events} from "../../../shared/Events"
 
 export interface StreamData {
-    name: string
-    playlistUrl: string
-    schedulePath: string
+    streamName?: string
+    playlistUrl?: string
+    schedulePath?: string
 }
 
-const emptyStreamData: StreamData = {name: "", playlistUrl: "", schedulePath: ""}
+const emptyStreamData: StreamData = {streamName: "", playlistUrl: "", schedulePath: ""}
 
 export interface AddStreamButtonProps {
 
 }
 
+export interface AddStreamButtonState {
+    isDialogOpen: boolean
+    streamData: StreamData
+    schedulePathText: string
+}
+
 function validateStreamData(streamData: StreamData): boolean {
     let isValid: boolean = false
-    if (streamData.name !== "" && streamData.playlistUrl !== "") isValid = true
+    if (streamData.streamName !== "" && streamData.playlistUrl !== "") isValid = true
     return isValid
 }
 
-export const AddStreamButton: FC<AddStreamButtonProps> = (props) => {
-    const [open, setOpen] = useState(false)
-    const [streamData, setData] = useState<StreamData>(emptyStreamData)
-    const [schedulePathText, setSchedulePathText] = useState<string>("")
+export class AddStreamButton extends Component<AddStreamButtonProps, AddStreamButtonState> {
 
-    const changeData = (changedData: { streamName?: string, playlistUrl?: string, schedulePath?: string }) => {
-        setData((prevState: StreamData) => {
-            const result: StreamData = prevState
-            if (changedData.streamName) result.name = changedData.streamName
-            if (changedData.playlistUrl) result.playlistUrl = changedData.playlistUrl
-            if (changedData.schedulePath) result.schedulePath = changedData.schedulePath
-            return result
-        })
-    }
-
-    const handleCancelled = () => {
-        setOpen(false)
-    }
-
-    const handleSubmit = async () => {
-        if (validateStreamData(streamData)) {
-            await sendToMain(Events.NewStream, streamData)
-            setData({name: "", playlistUrl: "", schedulePath: ""})
+    constructor(props: AddStreamButtonProps) {
+        super(props)
+        this.state = {
+            isDialogOpen: false,
+            streamData: emptyStreamData,
+            schedulePathText: ""
         }
-        setOpen(false)
     }
 
-    return (
-        <div>
-            <Button variant="outlined" color="primary" onClick={() => {setOpen(true)}}>
-                New Stream
-            </Button>
-            <Dialog open={open} onClose={handleCancelled}>
-                <DialogTitle>New Stream</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="streamName"
-                        label="Stream Name"
-                        fullWidth
-                        defaultValue={streamData.name}
-                        onChange={event => changeData({streamName: event.target.value})}
-                    />
-                    <TextField
-                        margin="dense"
-                        id="playlistUrl"
-                        label="Playlist Url"
-                        fullWidth
-                        defaultValue={streamData.playlistUrl}
-                        onChange={event => changeData({playlistUrl: event.target.value})}
-                    />
-                    <TextField
-                        margin="dense"
-                        id="schedulePath"
-                        label="Schedule Path"
-                        fullWidth
-                        defaultValue={streamData.schedulePath}
-                        value={schedulePathText}
-                        onChange={event => {
-                            setSchedulePathText(event.target.value)
-                            changeData({schedulePath: event.target.value})
-                        }}
-                    />
-                    <Button onClick={async () => {
-                        const result: string | undefined = await sendToMain<string>(Events.BrowseSchedule)
-                        if (result) {
-                            setSchedulePathText(result)
-                            changeData({schedulePath: result})
-                        }
-                    }}>Browse Schedule</Button>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleSubmit} color="primary">
-                        Add Stream
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    )
+    public render() {
+
+        const handleCancelled = () => {
+            this.setState({isDialogOpen: false})
+        }
+
+        const handleSubmit = async () => {
+            if (validateStreamData(this.state.streamData)) {
+                await sendToMain(Events.NewStream, this.state.streamData)
+                this.setState({streamData: {streamName: "", playlistUrl: "", schedulePath: ""}})
+            }
+            this.setState({isDialogOpen: false})
+        }
+
+        return (
+            <div>
+                <Button variant="outlined" color="primary" onClick={() => this.setState({isDialogOpen: true})}>
+                    New Stream
+                </Button>
+                <Dialog open={this.state.isDialogOpen} onClose={handleCancelled}>
+                    <DialogTitle>New Stream</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="streamName"
+                            label="Stream Name"
+                            fullWidth
+                            defaultValue={this.state.streamData.streamName}
+                            onChange={event => this.setState({streamData: {streamName: event.target.value}})}
+                        />
+                        <TextField
+                            margin="dense"
+                            id="playlistUrl"
+                            label="Playlist Url"
+                            fullWidth
+                            defaultValue={this.state.streamData.playlistUrl}
+                            onChange={event => this.setState({streamData: {streamName: event.target.value}})}
+                        />
+                        <TextField
+                            margin="dense"
+                            id="schedulePath"
+                            label="Schedule Path"
+                            fullWidth
+                            defaultValue={this.state.streamData.schedulePath}
+                            value={this.state.schedulePathText}
+                            onChange={event => {
+                                this.setState({schedulePathText: event.target.value})
+                                this.setState({streamData: {schedulePath: event.target.value}})
+                            }}
+                        />
+                        <Button onClick={async () => {
+                            const result: string | undefined = await sendToMain<string>(Events.BrowseSchedule)
+                            if (result) {
+                                this.setState({schedulePathText: result})
+                                this.setState({streamData: {schedulePath: result}})
+                            }
+                        }}>Browse Schedule</Button>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleSubmit} color="primary">
+                            Add Stream
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        )
+    }
 }
